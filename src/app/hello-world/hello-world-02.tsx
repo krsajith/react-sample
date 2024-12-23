fdb614763f493765e944346eda0ba7c5bf5f63ff
Sajith <krsajith@gmail.com>
2024-12-23 09:05:50 +0530

    Added nx created components with nx

---
+++ src/app/hello-world/hello-world-02.tsx
@@ -0,0 +1,14 @@
+
+
+export function HelloWorld02() {
+  return (
+    <div>
+      <h1>Welcome to HelloWorld02!</h1>
+    </div>
+  );
+}
+
+export default HelloWorld02;
+
+
+
---
+++ src/app/hello-world/hello-world.tsx
@@ -0,0 +1,14 @@
+
+
+export function HelloWorld() {
+  return (
+    <div>
+      <h1>Welcome to HelloWorld!</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
+
+
+
